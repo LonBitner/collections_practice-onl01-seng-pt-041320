e6647154ecf4e5ceedfd54577fecf968c93292a0
Lon Bitner <lonbitner@gmail.com>
2020-04-22 08:11:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
+def sort_array_asc(asc)
+  asc.sort
+end
 
+def sort_array_desc(desc)
+  desc.sort {|first_num, second_num| second_num <=> first_num}
+end
+
+def sort_array_char_count(count)
+  
+end
