fa5575e37a7dcd71b22b5a6b719e12923fb5c2f4
Lon Bitner <lonbitner@gmail.com>
2020-04-22 08:19:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,12 +15,3 @@ def swap_elements(strings)
   return strings
 end
 
-def swap_elements_from_to(array, index, destination_index)
-  array[index], array[destination_index] = array[destination_index], array[index]
-  return array
-end
-
-def reverse_array(array)
-  new_array = integers.reverse
-  new_array
-end
